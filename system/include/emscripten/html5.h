1bc5d36158cb3e46c5180e76220220b0d9de43c7
Jukka Jylänki <jujjyl@gmail.com>
2018-10-10 13:01:06 +0300

    Add emscripten_html5_remove_all_event_listeners() to allow unregistering all registered event handlers.

--- system/include/emscripten/html5.h
+++ system/include/emscripten/html5.h
@@ -444,6 +444,8 @@ extern EMSCRIPTEN_RESULT emscripten_get_canvas_element_size(const char *target, 
 extern EMSCRIPTEN_RESULT emscripten_set_element_css_size(const char *target, double width, double height);
 extern EMSCRIPTEN_RESULT emscripten_get_element_css_size(const char *target, double *width, double *height);
 
+extern void emscripten_html5_remove_all_event_listeners(void);
+
 #ifdef __cplusplus
 } // ~extern "C"
 #endif
